c861ba6976b2fa2bc90f1e1bc7b2036888b86c13
Todd A. Fisher <taf2@web3.anerian.com>
2009-06-13 10:24:07 -0400

    trim trailing whitespace

--- lib/typhoeus.rb
+++ lib/typhoeus.rb
@@ -1,5 +1,3 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__)) unless $LOAD_PATH.include?(File.dirname(__FILE__))
-
 require 'cgi'
 require 'digest/sha2'
 require 'typhoeus/easy'
@@ -13,22 +11,22 @@ require 'typhoeus/response'
 
 module Typhoeus
   VERSION = "0.0.9"
-  
+
   def self.easy_object_pool
     @easy_objects ||= []
   end
-  
+
   def self.init_easy_object_pool
     20.times do
       easy_object_pool << Typhoeus::Easy.new
     end
   end
-  
+
   def self.release_easy_object(easy)
     easy.reset
     easy_object_pool << easy
   end
-  
+
   def self.get_easy_object
     if easy_object_pool.empty?
       Typhoeus::Easy.new
@@ -36,12 +34,12 @@ module Typhoeus
       easy_object_pool.pop
     end
   end
-  
+
   def self.add_easy_request(easy_object)
     Thread.current[:curl_multi] ||= Typhoeus::Multi.new
     Thread.current[:curl_multi].add(easy_object)
   end
-  
+
   def self.perform_easy_requests
     Thread.current[:curl_multi].perform
   end
